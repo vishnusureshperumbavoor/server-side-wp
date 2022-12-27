d08e38608a418b005fa56d82cabd41f96b89523d
= <vishnusureshperumbavoor@gmail.com>
2022-12-27 19:08:30 +0530

    iop;hln

--- app.js
+++ app.js
@@ -8,10 +8,10 @@ const dotenv = require("dotenv")
 dotenv.config()
 const mongodb_uri = process.env.MONGODB_URI
 const PORT = process.env.PORT
-
 db.on("connected", () => {
   console.log("mongodb connection established");
 });
+
 db.on("error", (err) => {
   console.log(`mongodb connection error`);
   console.log(`${err}`);
@@ -27,6 +27,7 @@ const userSchema = new mongoose.Schema({
   name: String,
   username: String,
   password: String,
+  createdAt: { type: Date, default: Date.now },
 });
 
 const User = mongoose.model("User", userSchema);
@@ -35,11 +36,18 @@ app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
 
+app.get("/signup", (req, res) => {
+  res.sendFile(__dirname + "/signup.html");
+});
+
 app.post("/registration", urlencodedParser, (req, res) => {
   const user = new User(req.body);
-  db.collection("user").insertOne(user, (err, coll) => {
+  db.collection("userdetails").insertOne(user, (err, coll) => {
     if (err) console.log(`error ${err}`);
-    else res.send(`inserted successfully ${coll}`);
+    else{
+      console.log('successfully inserted');
+      res.send(`inserted successfully ${coll}`);
+    }
   });
 });
 
