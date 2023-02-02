91c475186c3e727e9f75e88f9936d5f4a01c5313
= <vishnusureshperumbavoor@gmail.com>
2023-02-02 22:47:48 +0530

    fileupload

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ const mongodb_uri = process.env.MONGODB_URI;
 const PORT = process.env.PORT;
 const cookieParser = require("cookie-parser");
 const session = require("express-session");
+const collection = require("./collections");
+const fileupload = require("express-fileupload");
 
 app.use(
   session({
@@ -20,24 +22,23 @@ app.use(
 );
 
 app.use(cookieParser());
+app.use(fileupload());
 
 app.set("view engine", "hbs");
 app.set("views", "");
 
-db.on("connected", () => {
-  console.log("mongodb connection established");
-});
-
-db.on("error", (err) => {
-  console.log(`mongodb connection error`);
-  console.log(`${err}`);
-});
 mongoose.set("strictQuery", false);
-
-mongoose.connect(mongodb_uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose
+  .connect(mongodb_uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then((res) => {
+    console.log("mongodb connection established");
+  })
+  .catch((err) => {
+    console.log(`error : ${err.message}`);
+  });
 
 const userSchema = new mongoose.Schema({
   name: String,
@@ -49,12 +50,12 @@ const userSchema = new mongoose.Schema({
 const User = mongoose.model("User", userSchema);
 
 app.get("/session", (req, res) => {
-  res.send(req.session.user)
+  res.send(req.session.user);
 });
 
-app.get("/registration", (req, res) => {
+app.get("/signup", (req, res) => {
   if (req.session.user) res.redirect("home", { user: req.session.user });
-  else res.sendFile(__dirname + "/index.html");
+  else res.sendFile(__dirname + "/signup.html");
 });
 
 app.get("/login", (req, res) => {
@@ -63,22 +64,35 @@ app.get("/login", (req, res) => {
 });
 
 app.get("/", (req, res) => {
-  if (req.session.user) res.render("home", { user: req.session.user });
+  if (req.session.user){
+    console.log(req.session.user._id);
+    res.render("home", { user: req.session.user });
+  }
   else res.redirect("/login");
 });
 
-app.post("/registration", urlencodedParser, (req, res) => {
+app.post("/signup", urlencodedParser, (req, res) => {
+  //console.log(req.files.aiimage);
+  const image = req.files.aiimage
   const user = new User(req.body);
-  db.collection("userdetails").insertOne(user, (err, coll) => {
+  db.collection(collection.USER_COLLECTIONS).insertOne(user, (err, coll) => {
     if (err) console.log(`error ${err}`);
     else {
-      console.log("successfully inserted");
-      res.redirect("/login");
+      console.log(coll);
+      image.mv("./images/" + coll.insertedId + ".jpg",(err)=>{
+        if(err){
+          console.log(`file upload error ${err}`);
+        }
+        else{
+          console.log("successfully uploaded the file");
+          res.redirect("/login");
+        }
+      });
     }
   });
 });
 app.post("/login", urlencodedParser, (req, res) => {
-  db.collection("userdetails").findOne(
+  db.collection(collection.USER_COLLECTIONS).findOne(
     { username: req.body.username },
     (err, user) => {
       if (err) return console.error(err);
@@ -92,7 +106,7 @@ app.post("/login", urlencodedParser, (req, res) => {
         res.redirect("/");
       } else {
         console.log("Invalid username or password");
-        res.redirect('/login')
+        res.redirect("/login");
       }
     }
   );
